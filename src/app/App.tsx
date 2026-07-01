ec41507af3fcd970590193060512929d7d8064bc
CodeKnight <paladinknight.master@gmail.com>
2026-07-01 15:36:35 -0400

    init project

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { StickyBoard } from '../sticky-notes/StickyBoard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <StickyBoard />
+    </div>
+  );
+}
+
+export default App;
